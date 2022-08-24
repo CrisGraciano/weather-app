9c21779933f8619df00153123a3e3ad93cbf6678
CrisGraciano <cristobalgraciano98@gmail.com>
2022-08-24 02:26:23 -0500

    javascript starting code

---
+++ assets/script.js
@@ -0,0 +1,42 @@
+var userFormEl = document.querySelector("#user-form");
+var cityInputEl = document.querySelector("#city");
+var searchHistoryContainerEl = document.querySelector("#city-history");
+
+var getCityWeatherDay = function(city) {
+    // change url to inculde city and required items
+    var apiUrl = //"enter api here"
+
+    // make api request
+    fetch(apiUrl)
+      .then(function(response) {
+        // if response works
+        if (response.ok) {
+            response.json().then(function(data) {
+                displayWeatherInfo(data, city);
+            });
+        } else {
+            alert('Error: No Data Found!');
+        }
+      })
+      .catch(function(error) {
+        alert("Unable to connect to weather service.");
+      });
+};
+
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+
+    var city = cityInputEl.ariaValueMax.trim();
+
+    if (city) {
+        getCityWeatherDay(city);
+        cityInputEl.value = "";
+    } else {
+        alert("Please enter a valid city!");
+    }
+    console.log(event);
+};
+
+var displayWeatherInfo = function() {
+
+};
